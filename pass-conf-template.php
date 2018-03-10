31460e2f0271383a2cc53782c91c00092f348775
Dowte <qianmiansen@outlook.com>
2018-03-10 14:26:41 +0800

    Update template config

--- pass-conf-template.php
+++ pass-conf-template.php
@@ -1,9 +1,16 @@
 <?php
 return [
     'components' => [
+        //db configure (required)
         'db' => [
             'class' => 'pass\db\%dbClass%\Connection',
             'dbDir' => DB_FILE_DIR,
+        ],
+        //generate default config (optional)
+        'generate' => [
+            'class' => 'pass\PasswordGenerate',
+            'length' => 12,
+            'level' => 3,
         ],
     ],
     'params' => [
