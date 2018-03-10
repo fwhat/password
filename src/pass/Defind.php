da4088b447741540c9210e9f9bb27aac21a369d8
Dowte <qianmiansen@outlook.com>
2018-03-10 22:32:12 +0800

    Update Defind.php

--- src/pass/Defind.php
+++ src/pass/Defind.php
@@ -9,11 +9,11 @@
 
 use Dowte\Password\pass\components\FileUtil;
 
-defined('PASS_ENV') or define('PASS_ENV', 'prod');
-defined('PASS_USER_CONF_DIR') or define('PASS_USER_CONF_DIR', __DIR__ . '/../../data/');
 defined('DB_DIR') or define('DB_DIR', FileUtil::realPath(__DIR__ . '/../../data/'));
 defined('DB_PASS') or define('DB_PASS', '');
-defined('SQLITE_FILE') or define('SQLITE_FILE', __DIR__ . '/../../data/pass.db');
+defined('PASS_ENV') or define('PASS_ENV', 'prod');
 defined('CONF_FILE') or define('CONF_FILE', FileUtil::realPath(__DIR__ . '/../../.pass-conf.yaml'));
+defined('SQLITE_FILE') or define('SQLITE_FILE', __DIR__ . '/../../data/pass.db');
 defined('CONF_FILE_TEMP') or define('CONF_FILE_TEMP', realpath(__DIR__ . '/../../pass-conf-template.yaml'));
 defined('ALFRED_CONF_FILE') or define('ALFRED_CONF_FILE', FileUtil::realPath(__DIR__ . '/../../data/.pass'));
+defined('PASS_USER_CONF_DIR') or define('PASS_USER_CONF_DIR', __DIR__ . '/../../data/');
